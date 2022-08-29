5b6f0ae319370a95934ea61e324994c91dde8918
João Victor Lima de SOuza <victor-souza1997@hotmail.com>
2022-08-29 07:45:07 -0300

    adiciona sensor de ambiente

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,27 @@
 Scheduler userScheduler; // to control your personal task
 painlessMesh mesh;
 
-// GPIO where the DS18B20 is connected to
-const int oneWireBus = 4;
+#define DHTPIN 4      // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11 // DHT 22 (AM2302)
 
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(oneWireBus);
-
-// Pass our oneWire reference to Dallas Temperature sensor
-DallasTemperature sensors(&oneWire);
+DHT dht(DHTPIN, DHTTYPE);
 
 String readDHTTemperature()
 {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // Read temperature as Celsius (the default)
-  sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
   // Read temperature as Fahrenheit (isFahrenheit = true)
-  // float t = dht.readTemperature(true);
+  float t = dht.readTemperature(true);
   // Check if any reads failed and exit early (to try again).
-  if (isnan(temperatureC))
+  if (isnan(t))
   {
     Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
   else
   {
-    Serial.println(temperatureC);
-    return String(temperatureC);
+    Serial.println(t);
+    return String(t);
   }
 }
 
